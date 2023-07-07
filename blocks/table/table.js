74251cf73e5e9335f596d9d993a2805f2a813437
Joshua Oransky <Josh@Cazzaran.com>
2023-07-07 10:56:03 -0700

    Added Table blocks Adjusted layout and design

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -4,27 +4,62 @@
  * https://www.hlx.live/developer/block-collection/table
  */
 
+import {
+  createEl,
+} from '../../scripts/scripts.js';
+
 function buildCell(rowIndex) {
   const cell = rowIndex ? document.createElement('td') : document.createElement('th');
   if (!rowIndex) cell.setAttribute('scope', 'col');
   return cell;
 }
 
-export default async function decorate(block) {
-  const table = document.createElement('table');
+export default async function decorate(blockEl) {
+  const tableEl = document.createElement('table');
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
-  table.append(thead, tbody);
-  [...block.children].forEach((child, i) => {
+  tableEl.append(thead, tbody);
+  let colspan = 1;
+  [...blockEl.children].forEach((child, i) => {
     const row = document.createElement('tr');
     if (i) tbody.append(row);
     else thead.append(row);
     [...child.children].forEach((col) => {
       const cell = buildCell(i);
+      if (colspan < i) {
+        colspan = i + 1;
+      }
       cell.innerHTML = col.innerHTML;
       row.append(cell);
     });
   });
-  block.innerHTML = '';
-  block.append(table);
+  blockEl.innerHTML = '';
+  blockEl.append(tableEl);
+
+  if (blockEl.classList.contains('searchable')) {
+    const searchRowEl = document.createElement('tr');
+    const searchCellEl = createEl('th', {
+      colspan,
+    }, '', searchRowEl);
+    const inputEl = createEl('input', {
+      type: 'text',
+      placeholder: 'Search',
+    }, '', searchCellEl);
+    inputEl.addEventListener('keyup', () => {
+      const filter = inputEl.value.toUpperCase();
+      const rowEls = tableEl.querySelectorAll('tbody > tr');
+      rowEls.forEach((rowEl) => {
+        const cellEl = rowEl.querySelector('td');
+        if (cellEl) {
+          const txtValue = cellEl.textContent || cellEl.innerText;
+          if (txtValue.toUpperCase().includes(filter)) {
+            rowEl.style.display = 'table-row';
+          } else {
+            rowEl.style.display = 'none';
+          }
+        }
+      });
+    });
+    thead.prepend(searchRowEl);
+  }
 }
